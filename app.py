9fce241aa64374f772ca8eb526a99f05508b1573
Prajay Reddy Minuka <54284068+mprajay999@users.noreply.github.com>
2020-05-15 22:39:43 +0530

    added rating calculator

--- app.py
+++ app.py
@@ -15,7 +15,12 @@ def predict():
     H=request.form.get('Homeground')
     T=request.form.get('Toss')
     P=Predict(float(A),float(B),float(H),float(T))
-    return render_template('Message.html',p=P)
+    return render_template('Message.html',p='Team A Has a Winning Probability of {} %'.format(P*100))
+
+@app.route('/calculate')
+def calculate():
+    return render_template('Message.html',m='Team A rating = (1-(0.1*position of team A on points table))+(No. of matches Team A has won Against Team B in the past / Total no. of Matches Played Between Team A and Team B)',
+                           m1='Team B rating = (1-(0.1*position of team B on points table))+(No. of matches Team B has won Against Team A in the past / Total no. of Matches Played Between Team A and Team B)')
 
 
 if __name__ == '__main__':
