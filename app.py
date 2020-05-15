93e60c97c8207cee701a3ac2f7d4482fdafc26c3
Prajay Reddy Minuka <54284068+mprajay999@users.noreply.github.com>
2020-05-15 22:48:37 +0530

    added calculate.html

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def predict():
 
 @app.route('/calculate')
 def calculate():
-    return render_template('Message.html',m='Team A rating = (1-(0.1*position of team A on points table))+(No. of matches Team A has won Against Team B in the past / Total no. of Matches Played Between Team A and Team B)',
+    return render_template('Calculate.html',m='Team A rating = (1-(0.1*position of team A on points table))+(No. of matches Team A has won Against Team B in the past / Total no. of Matches Played Between Team A and Team B)',
                            m1='Team B rating = (1-(0.1*position of team B on points table))+(No. of matches Team B has won Against Team A in the past / Total no. of Matches Played Between Team A and Team B)')
 
 
